f7d561a24c0d638b829b2be96bf867c05817bc75
Seungyong Yun <yo0230@gmail.com>
2015-09-11 19:27:02 +0900

    CollectionView UI 삽입

--- Swish/Controllers/PhotoPickerViewController.swift
+++ Swish/Controllers/PhotoPickerViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class PhotoPickerViewController: UIViewController {
+class PhotoPickerViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 90, height: 90)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView!)
     }
     
     override func didReceiveMemoryWarning() {
@@ -34,4 +43,27 @@ class PhotoPickerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Delegate
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.blackColor()
+//        cell.textLabel?.text = "\(indexPath.section):\(indexPath.row)"
+//        cell.imageView?.image = UIImage(named: "circle")
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+    {
+        return CGSizeMake((UIScreen.mainScreen().bounds.width-50)/3,90); //use height whatever you wants.
+    }
 }
