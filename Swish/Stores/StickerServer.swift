9c98cb39ae9e2923441a4c4a0b13def6b4b48099
Chris Jeong <crowjdh@gmail.com>
2015-10-09 11:17:56 +0900

    Swish 서버에서 스티커 파일의 url을 요청하는 로직 구현

---
+++ Swish/Stores/StickerServer.swift
@@ -0,0 +1,34 @@
+//
+//  StickerServer.swift
+//  Swish
+//
+//  Created by 정동현 on 2015. 10. 9..
+//  Copyright © 2015년 Yooii Studios Co., LTD. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+final class StickerServer {
+    private static let baseStickerUrl = SwishServer.host + "/stickers"
+    
+    class func stickerWith(key: String, onSuccess: (stickerUrl: String) -> (), onFail: FailCallback) {
+            let params = stickerParamWith(key)
+            let parser = { (resultJson: JSON) -> String in return stickerFileUrlFrom(resultJson) }
+            let httpRequest = HttpRequest<String>(method: .GET, url: baseStickerUrl, parameters: params, parser: parser, onSuccess: onSuccess, onFail: onFail)
+            
+            SwishServer.requestWith(httpRequest)
+    }
+    
+    // MARK: - Params
+    
+    private class func stickerParamWith(key: String) -> Param {
+        return [ "sticker_name": key ]
+    }
+    
+    // MARK: - Parsers
+    
+    private class func stickerFileUrlFrom(resultJson: JSON) -> String {
+        return resultJson["sticker_zipfile_url"].stringValue
+    }
+}
