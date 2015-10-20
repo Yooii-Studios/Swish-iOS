d2f9ac6187f3a90a861bdb5d84f31b3ba70e084e
Wooseong Kim <kwosu87@me.com>
2015-10-20 12:58:13 +0900

    ShareResultController 추가 및 segue 연결, 동현이 요청한 테스트 이미지뷰 추가

--- Swish/Controllers/DressingViewController.swift
+++ Swish/Controllers/DressingViewController.swift
@@ -32,14 +32,25 @@ class DressingViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showShareResult" {
+            // TODO: 여기서부터 초기화는 동현이 추후에 해줄 것
+//            let destinationController = segue.destinationViewController as! ShareResultController
+//            destinationController.image = image
+        }
     }
-    */
 
+    @IBAction func shareButtonDidTap(sender: AnyObject) {
+        // TODO: 나중에 사진 보내기 애니메이션 구현할 것
+        
+        // 애니메이션 없이 동현이 사진 보내기 로직을 구현하고, 
+        // 콜백에서 여기에 미리 만들어둔 ShareResultViewController
+        // 와의 연결 로직을 옮겨 준다. 
+        performSegueWithIdentifier("showShareResult", sender: nil)
+    }
+    
+    
 }
