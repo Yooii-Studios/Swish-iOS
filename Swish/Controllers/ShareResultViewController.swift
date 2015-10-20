8ddaead73a51d47ac198d3092340af6263e27573
Wooseong Kim <kwosu87@me.com>
2015-10-20 14:25:31 +0900

    Update ShareResultViewController.swift

--- Swish/Controllers/ShareResultViewController.swift
+++ Swish/Controllers/ShareResultViewController.swift
@@ -22,7 +22,6 @@ class ShareResultViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
