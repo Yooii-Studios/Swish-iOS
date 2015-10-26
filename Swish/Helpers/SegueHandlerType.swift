11b97f33593c4af71007ea390eadb278fe4e9ed5
Wooseong Kim <kwosu87@me.com>
2015-10-26 14:56:01 +0900

    SegueHandlerType 파일에 동현과 의논해서 기존의 주석을 지우고 usage를 추가하는 방향으로 주석 수정

--- Swish/Helpers/SegueHandlerType.swift
+++ Swish/Helpers/SegueHandlerType.swift
@@ -1,5 +1,28 @@
 //
 //  SegueHandlerType.swift
+//   UIViewController에서 segueIdentifier를 enum으로 래핑해서 쓸 수 있게 만들어주는 Protocol
+//
+//  1. UIViewController 및 sublcass 에 SegueHandlerType 프로토콜 추가
+//
+//  2. 해당 UIViewController 에 상단부에 사용될 enum SegueIdentifier 선언
+//
+//        enum SegueIdentifier: String {
+//            case ShowShareResult
+//        }
+//
+//  3. performSegueWithIdentifier() 메서드는 아래 래핑된 extension의 메서드를 대신 사용하고,
+//     prepareForSegue() 메서드는 다음과 같이 사용(from DressingViewController)
+//     segueIdentifierForSegue()도 extension에서 구현
+//
+//        switch segueIdentifierForSegue(segue) {
+//        case .ShowShareResult:
+//            // Do something
+//        }
+//
+//  자세한 내용은 WWDC 2015 세션 415 - Swift in Practice을 참고
+//  https://developer.apple.com/videos/play/wwdc2015-411/
+//
+//
 //  Swish
 //
 //  Created by Wooseong Kim on 2015. 10. 24..
@@ -8,60 +31,21 @@
 
 import UIKit
 
-/**
-    A protocol specific to the Lister sample that represents the segue identifier
-    constraints in the app. Every view controller provides a segue identifier
-    enum mapping. This protocol defines that structure.
-
-    We also want to provide implementation to each view controller that conforms
-    to this protocol that helps box / unbox the segue identifier strings to
-    segue identifier enums. This is provided in an extension of `SegueHandlerType`.
-*/
 protocol SegueHandlerType {
-    /**
-        Gives structure to what we expect the segue identifiers will be. We expect
-        the `SegueIdentifier` mapping to be an enum case to `String` mapping.
-
-        For example:
-
-        enum SegueIdentifier: String {
-        case ShowAccount
-        case ShowHelp
-        ...
-     }
-     */
+    
     typealias SegueIdentifier: RawRepresentable
 }
 
-/**
-    Constrain the implementation for `SegueHandlerType` conforming
-    types to only work with view controller subclasses whose `SegueIdentifier`
-    raw values are `String` instances. Practically speaking, the enum that provides
-    the mapping between the view controller's segue identifier strings should
-    be backed by a `String`. See the description for `SegueHandlerType` for an example.
-*/
 extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
-    /**
-        An overload of `UIViewController`'s `performSegueWithIdentifier(_:sender:)`
-        method that takes in a `SegueIdentifier` enum parameter rather than a
-        `String`.
-    */
+    
     func performSegueWithIdentifier(segueIdentifier: SegueIdentifier, sender: AnyObject?) {
         performSegueWithIdentifier(segueIdentifier.rawValue, sender: sender)
     }
- 
-    /**
-        A convenience method to map a `StoryboardSegue` to the  segue identifier
-        enum that it represents.
-    */
+
     func segueIdentifierForSegue(segue: UIStoryboardSegue) -> SegueIdentifier {
-        /*
-            Map the segue identifier's string to an enum. It's a programmer error
-            if a segue identifier string that's provided doesn't map to one of the
-            raw representable values (most likely enum cases).
-        */
         guard let identifier = segue.identifier,
                   segueIdentifier = SegueIdentifier(rawValue: identifier) else {
+            // TODO: 출시 전에 Crashylitics로 옮긴다거나 하는 부분 동현과 의논 필요
             fatalError("Couldn't handle segue identifier \(segue.identifier) for view controller of type \(self.dynamicType).")
         }
         
