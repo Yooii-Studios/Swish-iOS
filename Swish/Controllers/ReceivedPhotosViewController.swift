55d95909a57d86c581420f9267062aa251fffb01
Wooseong Kim <kwosu87@me.com>
2015-11-04 14:22:21 +0900

    ReceivedPhotosViewController 추가, 스토리보드에서 경고없게 UICollectionViewCell 프로토타입 완성
    - Content Hugging Priority 와 Content Compression Priority 에 대한 이해가 어느 정도 필요했음.
    free size에서 경고가 뜨지 않게 작업이 되었기에 여러 디바이스에 대응이 어느 정도 될 듯.
    우선 초기화 부분이 성공하게 되면 이후 디바이스 / 2 너비로 동적으로 맞추어 줄 수 있는 기능 추가가 필요

---
+++ Swish/Controllers/ReceivedPhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReceivedPhotosViewController.swift
+//  Swish
+//
+//  Created by Wooseong Kim on 2015. 11. 3..
+//  Copyright © 2015년 Yooii Studios Co., LTD. All rights reserved.
+//
+
+import UIKit
+
+class ReceivedPhotosViewController: UIViewController {
+    // TODO: 거리 레이블이 기준이 되어서 이 너비가 동적으로 정해진 다음, 이 것을 제외한 나머지 길이가 안드로이드의 match_parent + leftOf 조합처럼 활용할 수 있게 constraints 구성해볼 것. baseline은 유저 이름 레이블에 거리 레이블이 맞출 수 있게 할 것.
+}
