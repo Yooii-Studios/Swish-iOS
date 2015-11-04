e63033c8a1f03610cf5ea412e33ffe2da3e9239f
Wooseong Kim <kwosu87@me.com>
2015-11-04 23:45:16 +0900

    받은 사진들 화면 아이폰 6플에서 2열로 나오게 우선 구현 완료
    - 최소한도로 우선 구현. 동현과 async 이미지 로딩 관련 이슈 해결 필요. 동적으로 셀 너비, 높이 관련 계산 후 도입 로직 적용 필요.

--- Swish/Controllers/ReceivedPhotosViewController.swift
+++ Swish/Controllers/ReceivedPhotosViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ReceivedPhotosViewController: UIViewController {
-    // TODO: 거리 레이블이 기준이 되어서 이 너비가 동적으로 정해진 다음, 이 것을 제외한 나머지 길이가 안드로이드의 match_parent + leftOf 조합처럼 활용할 수 있게 constraints 구성해볼 것. baseline은 유저 이름 레이블에 거리 레이블이 맞출 수 있게 할 것.
+class ReceivedPhotosViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    // TODO: 디바이스 / 2 - 마진 = 한 셀의 너비가 되게 계산하고, wrap_content 처럼 높이가 자동적으로 정해질 수 있게 구현 필요
+    
+    @IBOutlet weak var photoCollectionView: UICollectionView!
     
     private var receivedPhotos: Array<Photo>!
     
@@ -17,7 +19,62 @@ class ReceivedPhotosViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         receivedPhotos = SwishDatabase.receivedPhotos()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        /*
+        if segue.identifier == "" {
+            let indexPaths = collectionView.indexPathsForSelectedItems()
+            let indexPath = indexPaths![0]
+            
+            let viewController = segue.destinationViewController as! ReceivedPhotoViewController
+        }
+        */
+    }
+    
+    // MARK: - UICollectionView DataSource
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return receivedPhotos.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = dequeueReusableCell(collectionView, atIndexPath: indexPath)
+        makeCleanCell(cell)
+        initCell(cell, atIndexPath: indexPath)
+        return cell
+    }
+    
+    private func dequeueReusableCell(collectionView: UICollectionView, atIndexPath indexPath: NSIndexPath) ->
+        ReceivedPhotoViewCell {
+            
+        return collectionView.dequeueReusableCellWithReuseIdentifier("receivedPhotoCell", forIndexPath: indexPath)
+            as! ReceivedPhotoViewCell
+    }
+    
+    private func makeCleanCell(cell: ReceivedPhotoViewCell) {
+        cell.imageView.image = nil
+        cell.userNameLabel.text = nil
+        cell.distanceLabel.text = nil
+        cell.messageLabel.text = nil
+    }
+    
+    private func initCell(cell: ReceivedPhotoViewCell, atIndexPath indexPath: NSIndexPath) {
+        let photo = receivedPhotos[indexPath.row]
+        PhotoImageHelper.imageWithPhoto(photo) { image in
+            cell.imageView.image = image
+            cell.userNameLabel.text = photo.sender.name
+            cell.messageLabel.text = photo.message
+            // TODO: 거리 관련 유틸리티 추후 구현해서 적용할 것
+            cell.distanceLabel.text = "6230 km"
+        }
+    }
+    
+    // MARK: - UICollectionView Delegate
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // TODO: SeugeHandlerType 구현하고 수정 필요
+//        self.performSegueWithIdentifier("", sender: self)
     }
 }
---
+++ Swish/Views/ReceivedPhotoViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReceivedPhotoViewCell.swift
+//  Swish
+//
+//  Created by Wooseong Kim on 2015. 11. 4..
+//  Copyright © 2015년 Yooii Studios Co., LTD. All rights reserved.
+//
+
+import UIKit
+
+class ReceivedPhotoViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+}
