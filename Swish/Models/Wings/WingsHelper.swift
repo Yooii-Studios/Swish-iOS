0a9459ecda06eb56a4839b2619dff91deb90d9dd
Chris Jeong <crowjdh@gmail.com>
2015-11-05 15:41:38 +0900

    Wings 클래스를 그룹으로 옮기고 이 클래스를 활용하는 헬퍼 클래스 추가

---
+++ Swish/Models/Wings/WingsHelper.swift
@@ -0,0 +1,13 @@
+//
+//  WingsHelper.swift
+//  Swish
+//
+//  Created by 정동현 on 2015. 11. 5..
+//  Copyright © 2015년 Yooii Studios Co., LTD. All rights reserved.
+//
+
+import Foundation
+
+final class WingsHelper {
+    // TODO: 안드로이드 구현 따라 날개를 다루는 작업들 구현
+}
