251cde548a4c2627f851cc8f09dda0e1791d74a6
Wooseong Kim <kwosu87@me.com>
2015-11-16 19:50:18 +0900

    탭을 선택할 때 네비게이션 바 이름을 변경해주게 구현

--- Swish/Controllers/MyInfoViewController.swift
+++ Swish/Controllers/MyInfoViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class MyInfoViewController: UITabBarController {
    
+    enum TabType: Int {
+        case Received
+        case Sent
+        case Profile
+    }
+    
     var dismissButton: UIButton!
     
     override func viewDidLoad() {
@@ -26,14 +32,28 @@ class MyInfoViewController: UITabBarController {
     @IBAction func cancelBarButtonDidTap() {
         dismissViewControllerAnimated(true, completion: nil)
     }
+
+    // MARK: - Navigation
     
     /*
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
     */
+    
+    override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        // TODO: 로컬라이징 필요
+        if let currentTabType = TabType(rawValue: selectedIndex) {
+            switch currentTabType {
+            case .Received:
+                title = "RECEIVED"
+            case .Sent:
+                title = "SENT"
+            case .Profile:
+                title = "PROFILE"
+            }
+        }
+    }
 }
