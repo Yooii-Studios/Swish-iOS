aa482cb504818384a2b5793cd05b2105598c32a3
Chris Jeong <crowjdh@gmail.com>
2015-11-24 16:17:53 +0900

    LocationTrackable 내의 LocationManager의 위치 사용 승인 상태 변경을 감지, 다시 승인 요청 혹은 위치 요청을 다시 시도하도록 구현
    - 위치 사용 승인 요청: Restricted -> NotDetermined
    - 위치 요청: NotDetermined or Restricted -> AuthorizedWhenInUse

--- Swish/Controllers/LocationTrackable.swift
+++ Swish/Controllers/LocationTrackable.swift
@@ -41,7 +41,42 @@ enum LocationTrackType {
     case Track
 }
 
-private func requestLocation(locationManager: CLLocationManager, locationTrackType: LocationTrackType) {
+private func checkLocationAuthorizationStatus(locationTrackable: LocationTrackable,
+    viewController: UIViewController) -> Bool{
+        guard CLLocationManager.locationServicesEnabled() else {
+            // TODO: 디바이스의 상태가 위치 서비스를 사용할 수 없는 상황. 예외처리가 필요시 예외처리 추가
+            print("locationService disabled")
+            return false
+        }
+        let status = CLLocationManager.authorizationStatus()
+        guard status != .NotDetermined else {
+            // TODO: 로컬라이징.유저에게 현재위치 사용 요청하는 다이얼로그를 띄워주는 부분.
+            // info.plist의 NSLocationWhenInUseUsageDescription 키에 해당하는 값을 다국어 번역해야함
+            print("Requesting location service...")
+            locationTrackable.locationTrackHandler.locationManager.requestWhenInUseAuthorization()
+            return false
+        }
+        guard status == .AuthorizedWhenInUse else {
+            alertLocationServiceWithViewController(viewController, status: status)
+            return false
+        }
+        return true
+}
+
+private func notifyOrRequestLocationUpdate(locationTrackable: LocationTrackable) {
+    // cache가 있고 one shot인 경우 위치를 요청할 필요가 없으므로 알린 후 끝냄
+    let locationTrackHandler = locationTrackable.locationTrackHandler
+    let locationTrackType = locationTrackable.locationTrackType
+    
+    if let recentLocation = locationTrackHandler.locationManager.location where locationTrackType == .OneShot {
+        print("recent location:\(recentLocation)")
+        locationTrackable.locationDidUpdate(recentLocation)
+    } else {
+        requestLocationUpdate(locationTrackHandler.locationManager, locationTrackType: locationTrackType)
+    }
+}
+
+private func requestLocationUpdate(locationManager: CLLocationManager, locationTrackType: LocationTrackType) {
     if #available(iOS 9.0, *), locationTrackType == .OneShot {
         locationManager.requestLocation()
     } else {
@@ -97,11 +132,13 @@ private func openSwishSystemSettings() {
 final class LocationTrackHandler: NSObject, CLLocationManagerDelegate {
     
     private var locationManager: CLLocationManager
+    private var previousAuthStatus: CLAuthorizationStatus
     weak private final var locationTrackable: LocationTrackable?
     
     init(delegate: LocationTrackable?) {
         self.locationTrackable = delegate
         self.locationManager = CLLocationManager()
+        self.previousAuthStatus = CLLocationManager.authorizationStatus()
         super.init()
         self.locationManager.delegate = self
     }
@@ -118,9 +155,26 @@ final class LocationTrackHandler: NSObject, CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print("locationManager didFailWithError: \(error)")
-        if let viewController = UIApplication.sharedApplication().keyWindow?.rootViewController {
+        if let viewController = currentRootViewController {
             alertUnknownErrorWithViewController(viewController)
         }
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if let locationTrackable = locationTrackable where status != previousAuthStatus {
+            if status == .NotDetermined || status == .Restricted {
+                if let viewController = currentRootViewController {
+                    checkLocationAuthorizationStatus(locationTrackable, viewController: viewController)
+                }
+            } else if status == .AuthorizedWhenInUse {
+                notifyOrRequestLocationUpdate(locationTrackable)
+            }
+        }
+        previousAuthStatus = status
+    }
+    
+    var currentRootViewController: UIViewController? {
+        return UIApplication.sharedApplication().keyWindow?.rootViewController
     }
 }
 
@@ -135,30 +189,10 @@ protocol LocationTrackable: class {
 extension LocationTrackable where Self: UIViewController {
     
     func requestLocationUpdate() {
-        guard CLLocationManager.locationServicesEnabled() else {
-            // TODO: 디바이스의 상태가 위치 서비스를 사용할 수 없는 상황. 예외처리가 필요시 예외처리 추가
-            print("locationService disabled")
+        guard checkLocationAuthorizationStatus(self, viewController: self) else {
             return
         }
-        let status = CLLocationManager.authorizationStatus()
-        guard status != .NotDetermined else {
-            // TODO: 로컬라이징.유저에게 현재위치 사용 요청하는 다이얼로그를 띄워주는 부분.
-            // info.plist의 NSLocationWhenInUseUsageDescription 키에 해당하는 값을 다국어 번역해야함
-            print("Requesting location service...")
-            locationTrackHandler.locationManager.requestWhenInUseAuthorization()
-            return
-        }
-        guard status == .AuthorizedWhenInUse else {
-            alertLocationServiceWithViewController(self, status: status)
-            return
-        }
-        // cache가 있고 one shot인 경우 위치를 요청할 필요가 없으므로 알린 후 끝냄
-        if let recentLocation = locationTrackHandler.locationManager.location where locationTrackType == .OneShot {
-            print("recent location:\(recentLocation)")
-            locationDidUpdate(recentLocation)
-        } else {
-            requestLocation(locationTrackHandler.locationManager, locationTrackType: locationTrackType)
-        }
+        notifyOrRequestLocationUpdate(self)
     }
     
     func stopUpdatingLocation() {
