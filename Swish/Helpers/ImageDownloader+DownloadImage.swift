a02b595b575546818c643cb3cc5d6b04b0e2f7a4
Chris Jeong <crowjdh@gmail.com>
2015-12-02 14:50:12 +0900

    AlamofireImage의 ImageDownloader를 이용해 downloadImage 메서드를 class function처럼 사용할 수 있게 래핑

---
+++ Swish/Helpers/ImageDownloader+DownloadImage.swift
@@ -0,0 +1,51 @@
+//
+//  ImageDownloader+DownloadImage.swift
+//  Swish
+//
+//  Created by 정동현 on 2015. 12. 2..
+//  Copyright © 2015년 Yooii Studios Co., LTD. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import AlamofireImage
+
+extension ImageDownloader {
+    
+    class func downloadImage(URLString: String, completion: UIImage? -> Void) {
+        let URLRequest = NSURLRequest(URL: NSURL(string: URLString)!)
+        
+        ImageDownloaderHolder.instance.imageDownloader.downloadImage(URLRequest: URLRequest) { response in
+            if let image = response.result.value {
+                completion(image)
+            }
+        }
+    }
+}
+
+private final class ImageDownloaderHolder {
+    
+    // MARK: - Singleton
+    
+    private struct Instance {
+        static var dispatchToken: dispatch_once_t = 0
+        static var instance: ImageDownloaderHolder?
+    }
+    
+    static var instance: ImageDownloaderHolder {
+        get {
+            dispatch_once(&Instance.dispatchToken) {
+                Instance.instance = ImageDownloaderHolder()
+            }
+            return Instance.instance!
+        }
+    }
+    
+    final var imageDownloader: ImageDownloader!
+    
+    // MARK: - Initializers
+    
+    private init() {
+        imageDownloader = ImageDownloader()
+    }
+}
