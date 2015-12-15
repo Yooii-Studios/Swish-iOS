c09a0b682595d58af0ed008fe555529a482c3cd4
Chris Jeong <crowjdh@gmail.com>
2015-12-15 18:12:04 +0900

    ChatObserver 내 변수 명명 변경

--- Swish/Models/ChatObserver.swift
+++ Swish/Models/ChatObserver.swift
@@ -12,29 +12,29 @@ import ReactKit
 final class ChatObserver {
     
     // KVO Streams
-    private static var _unreadMessageCountStreams = Dictionary<Photo.ID, (Stream<AnyObject?>, Int)>()
-    private static var _chatMessageStateStreams = Dictionary<ChatMessage, (Stream<AnyObject?>, Int)>()
+    private static var unreadMessageCountStreams = Dictionary<Photo.ID, (Stream<AnyObject?>, Int)>()
+    private static var chatMessageStateStreams = Dictionary<ChatMessage, (Stream<AnyObject?>, Int)>()
     
     final class func observeUnreadMessageCountStreamForPhoto(photo: Photo, handler: Int -> Void) {
-        if _unreadMessageCountStreams[photo.id] == nil {
-            _unreadMessageCountStreams[photo.id] = (KVO.startingStream(photo, "unreadMessageCount"), 1)
+        if unreadMessageCountStreams[photo.id] == nil {
+            unreadMessageCountStreams[photo.id] = (KVO.startingStream(photo, "unreadMessageCount"), 1)
         } else {
-            _unreadMessageCountStreams[photo.id]!.1++
+            unreadMessageCountStreams[photo.id]!.1++
         }
-        print("left unread message count observers: \(_unreadMessageCountStreams.count)")
-        _unreadMessageCountStreams[photo.id]!.0 ~> { unreadMessageCount in
+        print("left unread message count observers: \(unreadMessageCountStreams.count)")
+        unreadMessageCountStreams[photo.id]!.0 ~> { unreadMessageCount in
             handler(unreadMessageCount as! Int)
         }
     }
     
     final class func observeChatMessageStateStream(chatMessage: ChatMessage, handler: ChatMessageSendState -> Void) {
-        if _chatMessageStateStreams[chatMessage] == nil {
-            _chatMessageStateStreams[chatMessage] = (KVO.startingStream(chatMessage, "stateRaw"), 1)
+        if chatMessageStateStreams[chatMessage] == nil {
+            chatMessageStateStreams[chatMessage] = (KVO.startingStream(chatMessage, "stateRaw"), 1)
         } else {
-            _chatMessageStateStreams[chatMessage]!.1++
+            chatMessageStateStreams[chatMessage]!.1++
         }
-        print("left chat state observers: \(_chatMessageStateStreams.count)")
-        _chatMessageStateStreams[chatMessage]!.0 ~> { rawState in
+        print("left chat state observers: \(chatMessageStateStreams.count)")
+        chatMessageStateStreams[chatMessage]!.0 ~> { rawState in
             handler(ChatMessageSendState(rawValue: rawState as! String)!)
         }
     }
@@ -42,22 +42,22 @@ final class ChatObserver {
     // TODO: count변경시 사진 정렬 위치 바꾸기 위한 스트림
     
     final class func unobserveUnreadMessageCountStream(photoId: Photo.ID) {
-        _unreadMessageCountStreams[photoId]?.0.cancel()
-        _unreadMessageCountStreams[photoId]?.1--
+        unreadMessageCountStreams[photoId]?.0.cancel()
+        unreadMessageCountStreams[photoId]?.1--
         
-        if _unreadMessageCountStreams[photoId]?.1 <= 0 {
-            _unreadMessageCountStreams[photoId] = nil
+        if unreadMessageCountStreams[photoId]?.1 <= 0 {
+            unreadMessageCountStreams[photoId] = nil
         }
-        print("left unread message count observers: \(_unreadMessageCountStreams.count)")
+        print("left unread message count observers: \(unreadMessageCountStreams.count)")
     }
     
     final class func unobserveChatMessageStateStream(chatMessage: ChatMessage) {
-        _chatMessageStateStreams[chatMessage]?.0.cancel()
-        _chatMessageStateStreams[chatMessage]?.1--
+        chatMessageStateStreams[chatMessage]?.0.cancel()
+        chatMessageStateStreams[chatMessage]?.1--
         
-        if _chatMessageStateStreams[chatMessage]?.1 <= 0 {
-            _chatMessageStateStreams[chatMessage] = nil
+        if chatMessageStateStreams[chatMessage]?.1 <= 0 {
+            chatMessageStateStreams[chatMessage] = nil
         }
-        print("left chat state observers: \(_chatMessageStateStreams.count)")
+        print("left chat state observers: \(chatMessageStateStreams.count)")
     }
 }
