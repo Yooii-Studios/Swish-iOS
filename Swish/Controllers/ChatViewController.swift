d9a8ce3692f9e11f93b58621697024e1fa7b7563
Wooseong Kim <kwosu87@me.com>
2016-02-01 19:49:27 +0900

    채팅 VC 기본 틀 추가

---
+++ Swish/Controllers/ChatViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ChatViewController.swift
+//  Swish
+//
+//  Created by Wooseong Kim on 2016. 2. 1..
+//  Copyright © 2016년 Yooii Studios Co., LTD. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UITableViewController {
+
+    var photo: Photo!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        self.title = photo.sender.name
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - IBAction
+    
+    @IBAction func backButtonDidTap() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
