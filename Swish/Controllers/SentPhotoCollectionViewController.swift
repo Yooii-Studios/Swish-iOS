b7110f8759340ee86aa02f43e01aa34f6275434f
Wooseong Kim <kwosu87@me.com>
2016-02-16 15:57:35 +0900

    SentPhotoStateLoader 사용해서 메인 화면에 들어올 때 보낸 사진들의 상태 갱신하는 로직 추가

--- Swish/Controllers/SentPhotoCollectionViewController.swift
+++ Swish/Controllers/SentPhotoCollectionViewController.swift
@@ -21,6 +21,11 @@ class SentPhotoCollectionViewController: UIViewController {
         super.viewDidLoad()
         sentPhotos = SwishDatabase.sentPhotos()
         adjustCollectionViewCellSize()
+        refreshPhotoStates()
+    }
+    
+    private func refreshPhotoStates() {
+        SentPhotoStateLoader.instance.execute()
     }
     
     private func adjustCollectionViewCellSize() {
@@ -32,6 +37,12 @@ class SentPhotoCollectionViewController: UIViewController {
         (photoCollectionView.collectionViewLayout as! UICollectionViewFlowLayout).itemSize =
             CGSize(width: itemWidth, height: itemWidth * 1.3)
     }
+    
+    deinit {
+        SentPhotoStateLoader.instance.cancel()
+    }
+    
+    // MARK: - Navigation 
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         guard let cell = sender as? UICollectionViewCell, let indexPath = photoCollectionView.indexPathForCell(cell) else {
