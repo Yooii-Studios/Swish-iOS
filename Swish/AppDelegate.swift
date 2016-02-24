560ffa29c3aa8f2fdaa60b98ce7ed1caf6cb7c35
Seungyong Yun <yo0230@gmail.com>
2016-02-24 19:12:10 +0900

    applicationDidBecomeActive에서 DB에 접근함으로써 발생하는 문제 수정

--- Swish/AppDelegate.swift
+++ Swish/AppDelegate.swift
@@ -52,11 +52,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        MeManager.fetchMyUnreadChatMessages()
     }
     
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        MeManager.fetchMyUnreadChatMessages()
     }
     
     func applicationWillTerminate(application: UIApplication) {
