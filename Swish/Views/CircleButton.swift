e6d9bf4673ea4c032dbbc477e8618cd83bde6f21
Wooseong Kim <kwosu87@me.com>
2016-03-31 16:01:11 +0900

    우선 터치 없는 CircleButton 까지는 완성. 기존 코드를 거의 덜어내고 프로그래밍으로 거의 작업을 많이 하게 수정

--- Swish/Views/CircleButton.swift
+++ Swish/Views/CircleButton.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SwiftyColor
 import NibDesignable
+import UIColor_Hex_Swift
 
 // TODO: 추후 원형 버튼으로 만든 후 전체적인 색, 그림자 관련 로직 수정해줄 것
 @IBDesignable
@@ -17,34 +18,30 @@ class CircleButton: NibDesignable {
     struct Color {
         static let unselectedBackground = 0xe1e1e1~
         static let disabled = 0xebebeb~
+        static let shadow = UIColor(rgba: "#00000028")
     }
     
-    @IBOutlet var backgroundView: UIView!
-    @IBOutlet var foregroundView: UIView!
-    @IBOutlet var imageView: UIImageView!
-    
-    @IBOutlet var leadingConstraint: NSLayoutConstraint!
-    @IBOutlet var topConstraint: NSLayoutConstraint!
-    @IBOutlet var trailingConstraint: NSLayoutConstraint!
-    @IBOutlet var bottomConstraint: NSLayoutConstraint!
-    @IBOutlet var imageSizeConstraint: NSLayoutConstraint!
-    
-    @IBInspectable var backgroundCircleColor: UIColor! {
+    @IBInspectable var borderWidth: CGFloat = 0
+    @IBInspectable var circleBackgroundColor: UIColor! = UIColor.clearColor()
+    @IBInspectable var circleForegroundColor: UIColor! = UIColor.clearColor()
+    @IBInspectable var dropShadowEnabled: Bool = false {
         didSet {
-            backgroundView.backgroundColor = backgroundCircleColor
+            if dropShadowEnabled {
+                layer.masksToBounds = false
+                layer.shadowColor = Color.shadow.CGColor
+                layer.shadowOffset = CGSizeMake(1, 1);
+                layer.shadowOpacity = 1;
+                layer.shadowRadius = 1.0
+            }
         }
     }
-    @IBInspectable var foregroundCircleColor: UIColor! {
+    @IBInspectable var innerShadowEnabled: Bool = false
+    @IBInspectable var imageSize: CGFloat = 0 {
         didSet {
-            foregroundView.backgroundColor = foregroundCircleColor
-        }
-    }
-    @IBInspectable var strokeWidth: CGFloat = 8.0 {
-        didSet {
-            leadingConstraint.constant = strokeWidth
-            topConstraint.constant = strokeWidth
-            trailingConstraint.constant = strokeWidth
-            bottomConstraint.constant = strokeWidth
+            let x = self.frame.width / 2 - imageSize / 2
+            let y = self.frame.height / 2 - imageSize / 2
+            let frame = CGRect.init(x: x, y: y, width: imageSize, height: imageSize)
+            imageView.frame = frame
         }
     }
     @IBInspectable var image: UIImage! = UIImage() {
@@ -52,46 +49,80 @@ class CircleButton: NibDesignable {
             imageView.image = image
         }
     }
-    @IBInspectable var imageSize: CGFloat = 40 {
-        didSet {
-            imageSizeConstraint.constant = imageSize
-        }
+    private var imageView: UIImageView!
+    @IBInspectable var touchEffectEnabled: Bool = false
+    private var isTouchesBegan = false
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        _init()
     }
-    /*
-    @IBInspectable var enableShadow: Bool = false {
-        didSet {
-            // TODO: 그림자 추후 구현 예정
-        }
+    
+    private func _init() {
+        backgroundColor = UIColor.clearColor()
+        initImageView()
     }
-    */
+    
+    private func initImageView() {
+        imageView = UIImageView()
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        addSubview(imageView)
+    }
+
     
     final func setSelected(isSelected: Bool) {
-        if isSelected {
-            backgroundView.backgroundColor = backgroundCircleColor
-        } else {
-            backgroundView.backgroundColor = Color.unselectedBackground
-        }
+//        if isSelected {
+//            backgroundView.backgroundColor = backgroundCircleColor
+//        } else {
+//            backgroundView.backgroundColor = Color.unselectedBackground
+//        }
     }
     
     final func setDisabled() {
-        userInteractionEnabled = false
-        foregroundCircleColor = Color.disabled
-        backgroundCircleColor = Color.disabled
+//        userInteractionEnabled = false
+//        foregroundCircleColor = Color.disabled
+//        backgroundCircleColor = Color.disabled
     }
     
-    /*
-    // TODO: 그림자 관련 작업 시 필요해질 것 같아서 남김
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-    override func prepareForInterfaceBuilder() {
-        if image.size == CGSizeZero {
-            let bundle = NSBundle(forClass: self.dynamicType)
-            image = UIImage(named: "ic_photo_like_inactive", inBundle: bundle, compatibleWithTraitCollection: nil)
+        drawCircle(rect, withColor: circleBackgroundColor)
+        
+        let insetRect = rect.insetBy(dx: borderWidth, dy: borderWidth)
+        if !isTouchesBegan || !touchEffectEnabled {
+            if innerShadowEnabled {
+                let shadowRect = insetRect.offsetBy(dx: 1, dy: 1)
+                drawCircle(shadowRect, withColor: Color.shadow)
+            }
+            drawCircle(insetRect, withColor: circleForegroundColor)
         }
+    }
+    
+    private func drawCircle(rect: CGRect, withColor color: UIColor) {
+        color.setFill()
+        let circlePath = UIBezierPath(ovalInRect: rect)
+        circlePath.fill()
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if touches.first != nil {
+            isTouchesBegan = true
+            setNeedsDisplay()
+        }
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if touches.first != nil {
+            isTouchesBegan = false
+            setNeedsDisplay()
+        }
+        super.touchesEnded(touches, withEvent: event)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+//        if image.size == CGSizeZero {
+//            let bundle = NSBundle(forClass: self.dynamicType)
+//            image = UIImage(named: "ic_photo_like_inactive", inBundle: bundle, compatibleWithTraitCollection: nil)
+//        }
     }
 }
